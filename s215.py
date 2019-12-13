eab8b54b127e07c733505de770356954d302dc9c
Bobrovsky Dmitry <ministrbob777@gmail.com>
2019-12-13 12:42:27 +0300

    save

---
+++ s215.py
@@ -0,0 +1,43 @@
+import math
+import time
+from selenium import webdriver
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_css_selector('input.form-control.first[required]')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector('input.form-control.second[required]')
+    input2.send_keys("Petrov")
+    input2 = browser.find_element_by_css_selector('input.form-control.third[required]')
+    input2.send_keys("www@mail.ru")
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
